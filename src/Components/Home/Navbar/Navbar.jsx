726f8663a6b4d73130ddf2a6db402b268f6fcd23
Jisan Evo~ <jesanmahmud6@gmail.com>
2024-11-02 01:30:58 +0600

    firebase deploy sitting done

--- src/Components/Home/Navbar/Navbar.jsx
+++ src/Components/Home/Navbar/Navbar.jsx
@@ -1,11 +1,10 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../AuthProvider/AuthProvider";
-
+import { FiAlignJustify } from "react-icons/fi";
 
 const Navbar = () => {
-    // Ref to the service section
-    const {user,logOut}=useContext(AuthContext)
+    const { user, logOut } = useContext(AuthContext);
 
     return (
         <div>
@@ -13,58 +12,76 @@ const Navbar = () => {
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            {/* <img src="/logo.png" alt="" /> */}
+                            <div className="lg:hidden text-xl">
+                                <FiAlignJustify />
+                            </div>
                         </div>
                         <ul
                             tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            <Link to='/'>  <li className="text-black"><a>হোম</a></li></Link>
-
-                            <Link to='/service'><li className="text-black"><a>সার্ভিস</a></li></Link>
-                            <Link to='/reserve'><li className="text-black"><a>রিজার্ভ </a></li></Link>
-                            <Link to="/contact"><li className="text-black"><a>যোগাযোগ </a></li></Link>;                          {/* <li><a>Item 3</a></li> */}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                        >
+                            <Link to='/'>
+                                <li className="text-black"><a>হোম</a></li>
+                            </Link>
+                            <Link to='/service'>
+                                <li className="text-black"><a>সার্ভিস</a></li>
+                            </Link>
+                            <Link to='/reserve'>
+                                <li className="text-black"><a>রিজার্ভ </a></li>
+                            </Link>
+                            <Link to="/contact">
+                                <li className="text-black"><a>যোগাযোগ </a></li>
+                            </Link>
                         </ul>
                     </div>
-                    <a className="btn btn-ghost w-20 "><img src="/logo.png" alt="" /></a>
+                    <a className="w-14">
+                     <div className="">
+                     <img src="/logo.png" alt="" />
+                     </div>
+                    </a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <Link to='/'>  <li className="text-black"><a>হোম</a></li></Link>
-                        <Link to='/service'><li className="text-black"><a>সার্ভিস</a></li></Link>
-                        <Link to='/reserve'><li className="text-black"><a>রিজার্ভ </a></li></Link>
-                        <Link to="/contact"><li className="text-black"><a>যোগাযোগ </a></li> </Link>
+                        <Link to='/'>
+                            <li className="text-black"><a>হোম</a></li>
+                        </Link>
+                        <Link to='/service'>
+                            <li className="text-black"><a>সার্ভিস</a></li>
+                        </Link>
+                        <Link to='/reserve'>
+                            <li className="text-black"><a>রিজার্ভ </a></li>
+                        </Link>
+                        <Link to="/contact">
+                            <li className="text-black"><a>যোগাযোগ </a></li>
+                        </Link>
                     </ul>
                 </div>
                 <div className="navbar-end">
-                {
-                        user ? <div title={user?.displayName}  className="dropdown dropdown-end z-40"  >
+                    {user ? (
+                        <div title={user?.displayName} className="dropdown dropdown-end z-40">
                             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                                 <div className="w-10 rounded-full">
-                                    <img src={user?.photoURL||"https://i.postimg.cc/QNkbjGzf/images.png" } alt={user.displayName} />
+                                    <img src={user?.photoURL || "https://i.postimg.cc/QNkbjGzf/images.png"} alt={user.displayName} />
                                 </div>
                             </label>
                             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                                 <li>
-                                    <button className="btn btn-sm  btn-ghost">{user?.displayName || "Jisan"}</button>
-
+                                    <button className="btn btn-sm btn-ghost">{user?.displayName || "User"}</button>
                                 </li>
                                 <li>
-                                    <button className="btn btn-sm  btn-ghost"
-                                        onClick={() => logOut()}
-                                    >Logout</button>
-
+                                    <button className="btn btn-sm btn-ghost" onClick={() => logOut()}>
+                                        Logout
+                                    </button>
                                 </li>
                             </ul>
                         </div>
-                            :
-                            <Link to='/login'>
-                                <button className="btn btn-sm  btn-ghost">Login</button>
-                            </Link>
-                    }
+                    ) : (
+                        <Link to='/login'>
+                            <button className="btn btn-sm btn-ghost">Login</button>
+                        </Link>
+                    )}
                 </div>
             </div>
-
-
         </div>
     );
 };
